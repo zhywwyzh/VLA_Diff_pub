d45d8cbb5e382465b9724db5c465be1cf8bf0239
zhywwyzh <953059545@qq.com>
2025-08-26 13:54:27 +0800

    📃 docs: 删除ignore

--- simulation/EGO-Planner-v3/devel/share/decomp_ros_msgs/cmake/decomp_ros_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(decomp_ros_msgs_MSG_INCLUDE_DIRS "/home/zhywwyzh/workspace/VLA_Diff/simulation/EGO-Planner-v3/src/planner/uav_simulator/fake_target/DecompROS/decomp_ros_msgs/msg")
-set(decomp_ros_msgs_MSG_DEPENDENCIES geometry_msgs;nav_msgs;std_msgs)
